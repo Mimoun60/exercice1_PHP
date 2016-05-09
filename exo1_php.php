7404c2d91818a6705f16427153ad9ed3a42e727f
Mimoun60 <mimoun.boulashoub@bbox.fr>
2016-05-09 14:28:57 +0200

    exo1_PHP

---
+++ exo1_php.php
@@ -0,0 +1,24 @@
+<!doctype html>
+
+
+<html>
+<head>
+  <title></title>
+  <meta charset="utf-8">
+
+
+</head>
+
+<body>
+  <?php
+    $premiernombre= 6;
+    $secondnombre= 3;
+    $résultat= $premiernombre + $secondnombre;
+    echo $résultat;
+    echo "<br>";
+    echo $résultat *2;
+   ?>
+
+</body>
+
+</html>
